df3fc02b1983a389dcaf380a9c25a1a8d9a0b671
vulcao <vulcao@gmail.com>
2021-11-23 23:34:55 -0300

    proxima é #23

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <Navbar />
+  <Navbar :logo="logo_src" :alt="app_name" />
   <router-view/>
-  <Footer />
+  <Footer :alt="app_name" />
 </template>
 <script>
 import Navbar from './components/Navbar'
@@ -11,10 +11,31 @@ export default {
   components: {
     Navbar,
     Footer
+  },
+  data() {
+    return{
+      logo_src: "/img/logo.png",
+      app_name: "Mak ya burgah"
+    }
   }
 }
 </script>
 
 <style>
-
+* {
+  font-family: Arial, Helvetica, sans-serif;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+.main-container {
+  margin: 50px;
+  min-height: 250px;
+}
+h1 {
+  text-align: center;
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: #222;
+}
 </style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,24 @@
 <template>
-    <div id="footer">
-        <p>footer</p>
-    </div>
+    <footer id="footer">
+        <p>{{alt}} &copy;</p>
+    </footer>
 </template>
 <script>
 export default {
-    name: "Footer"
+    name: "Footer",
+    props: ["alt"]
 }
 </script>
 <style scoped>
-
+#footer {
+    height: 200px;
+    background-color: #222;
+    border-top: 4px solid #111;
+    color: #FCBA03;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    bottom: 0;
+    width: 100%;
+}
 </style>
