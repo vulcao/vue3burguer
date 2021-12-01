4c5518add31eb9698e04d082ac798f52cc900ac0
vulcao <vulcao@gmail.com>
2021-12-01 18:02:20 -0300

    #32

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="burguer-table">
+        <Message :mensagem="msg" :tipo="msgTipo" v-show="msg" />
         <div>
             <div id="burguer-table-heading">
                 <div class="order-id">#:</div>
@@ -11,30 +12,100 @@
             </div>
         </div>
         <div id="burguer-table-rows">
-            <div class="burguer-table-row">
-                <div class="order-id">1</div>
-                <div>Teste</div>
-                <div>Pão Teste</div>
-                <div>Carne Teste</div>
+            <div class="burguer-table-row" v-for="burger in burgers" :key="burger.id">
+                <div class="order-id">{{ burger.id }}</div>
+                <div>{{ burger.nome }}</div>
+                <div>{{ burger.pao }}</div>
+                <div>{{burger.carne }}</div>
                 <div>
                     <ul>
-                        <li>Salame</li>
-                        <li>Tomate</li>
+                        <li v-for="(opcional,index) in burger.opcionais" :key="index">
+                            {{ opcional }}
+                        </li>
                     </ul>
                 </div>
-                <div>
-                    <select name="status" class="status">
-                        <option value="">Selecione</option>
+                <div class="acoes">
+                    <select name="status" class="status"
+                    @change="updateBurger($event,burger.id)">
+                        <option>Selecione</option>
+                        <option v-for="statu in status" 
+                        :key="statu.id" 
+                        :value="statu.tipo"
+                        :selected="burger.status == statu.tipo">
+                            {{statu.tipo}}
+                        </option>
                     </select>
-                    <button class="delete-btn">Cancelar</button>
+                    <button class="delete-btn"
+                    @click="deleteBurger(burger.id)">
+                    Cancelar
+                    </button>
                 </div>
             </div>
         </div>
     </div>
 </template>
 <script>
+import Message from "../components/Message"
 export default{
     name: "Dashboard",
+    components: {
+        Message
+    },
+    data() {
+        return {
+            burgers: null,
+            burger_id: null,
+            status: [],
+            msg: null,
+            msgTipo: null
+        }
+    },
+    methods: {
+        async getPedidos() {
+            const req = await fetch("http://localhost:3000/burgers");
+            const data = await req.json();
+            this.burgers = data;
+        },
+        async getStatus() {
+            const req = await fetch("http://localhost:3000/status");
+            const data = await req.json();
+            this.status = data;
+        },
+        async deleteBurger(id) {
+            console.log(id)
+            const req = await fetch(`http://localhost:3000/burgers/${id}`,{
+                method: "DELETE"
+            });
+            const res = await req.json();
+            // mensagem
+            this.msg = `Pedido nº ${id} foi cancelado`;
+            this.msgTipo = "vermelho"
+            setTimeout(() => this.msg = "",3000);
+            // recarrega lista
+            this.getPedidos();
+        },
+        async updateBurger(event,id){
+            console.log(event)
+            console.log(id)
+            const option = event.target.value;
+            const dataJson = JSON.stringify({ status: option });
+            const req = await fetch(`http://localhost:3000/burgers/${id}`,{
+                method: "PATCH",
+                headers: { "Content-Type": "application/json" },
+                body: dataJson
+            });
+            // mensagem
+            this.msg = `Pedido nº ${id} foi alterado`;
+            this.msgTipo = "verde"
+            setTimeout(() => this.msg = "",3000);
+            // recarrega lista
+            this.getPedidos();
+        }
+    },
+    mounted() {
+        this.getPedidos();
+        this.getStatus();
+    }
 }
 </script>
 <style scoped>
@@ -55,7 +126,7 @@ flex-wrap: wrap;
 }
 #burguer-table-heading div,
 .burguer-table-row div {
-    width: 19%;
+    width: 18%;
 }
 .burguer-table-row  {
     width: 100%;
@@ -63,6 +134,11 @@ flex-wrap: wrap;
     border-bottom: 1px solid #ccc;
 }
 
+#burguer-table-heading .acoes,
+#burguer-table-rows .acoes {
+    width: 21%;
+
+}
 #burguer-table-heading .order-id,
 #burguer-table-rows .order-id {
     width: 5%;
@@ -70,7 +146,7 @@ flex-wrap: wrap;
 }
 select {
     padding: 10px 6px;
-    margin-right: 12px;
+    margin-right: 10px;
     font-size: 14px;
 }
 .status {
--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="message-container">
+    <div :class="tipo">
         <p>{{ mensagem }}</p>
     </div>
 </template>
@@ -7,18 +7,31 @@
 export default {
     name: "Message",
     props: {
-        mensagem: String
+        mensagem: String,
+        tipo: String
     }
 }
 </script>
 <style scoped>
-.message-container {
-    color:#004085;
-    background-color: #CCE5FF;
-    border: 2px solid #BBDAFF;
+div {
     border-radius: 5px;
     padding: 10px;
     max-width: 400px;
     margin: 30px auto;
+}
+.azul {
+    color:#004085;
+    background-color: #CCE5FF;
+    border: 2px solid #BBDAFF;
+}
+.vermelho {
+    color:#850000;
+    background-color: #ffe0cc75;
+    border: 2px solid #d4d2d242;
+}
+.verde {
+    color:#00851d;
+    background-color: #ccffe7b7;
+    border: 2px solid #bfd6c342;
 }
 </style>
