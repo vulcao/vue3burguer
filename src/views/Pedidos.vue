f537d3202be6cb98c11d8feb15fc9bdf233eed3d
vulcao <vulcao@gmail.com>
2021-12-01 15:24:56 -0300

    do #28

--- src/views/Pedidos.vue
+++ src/views/Pedidos.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="main-container">
-    <h1>Pedidos</h1>
+    <h1>Gerenciar Pedidos:</h1>
+    <Dashboard />
   </div>
 </template>
 <script>
-
+import Dashboard from "../components/Dashboard.vue";
 export default {
-  name: "Pedidos"
+  name: "Pedidos",
+  components: {
+    Dashboard
+  }
 }
 </script>
